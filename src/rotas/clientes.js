5855009b1034a738b272a606dfef29bded4fff67
zeckborges <quielborges@hotmail.com>
2022-10-26 10:08:04 -0300

    Cadastro de clientes e criação do arquivo beneficiarios.json

--- src/rotas/clientes.js
+++ src/rotas/clientes.js
@@ -1,6 +1,18 @@
 'use strict'
+const { create } = require('domain')
+const fs = require('fs')
 
 const init = (expressInstance, basePath) => {
+    expressInstance.post(`${basePath}register/cliente`, (req, res) => {
+        const cliente = {
+            nome: req.body.nome,
+            idade: req.body.idade,
+            plano: req.body.plano
+        }
+
+        fs.writeFileSync('beneficarios.json', JSON.stringify(cliente, null, 2))
+    }),
+
     expressInstance.get(`${basePath}clientes`, (req, res) => {
         res.send('Clientes')
     })
